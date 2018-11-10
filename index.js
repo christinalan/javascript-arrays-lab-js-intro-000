c1661f28c81109d696e24f6063248f9d53a4fdd1
Christina Lan <christina.lan13@gmail.com>
2018-11-10 14:05:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield']
+ //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(array, kitten) {
+  kittens.push(array)
+  return array
+}
+function destructivelyPrependKitten(array,kitten) {
+  kittens.unshift(array)
+  return array
+}
+function destructivelyRemoveLastKitten(array,kitten) {
+  kittens.pop(array)
+  return array
+}
+function destructivelyRemoveFirstKitten(array,kitten) {
+  kittens.shift(array)
+  return array
+}
+function appendKitten(array,kitten) {
+  [array, ...appendKitten]
+  return array
+}
